d2730a2fb51d0365190527a43b14a9ec000f07c6
片岡潤隆 <>
2022-01-21 03:34:11 +0900

    ヘッダーコンポーネントの作成

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,27 +1,21 @@
 import Link from "next/link";
+import styles from "src/components/header/index.module.scss";
+import { Nav } from "../Nav";
 
 export const Header = (): JSX.Element => {
   return (
-    <header>
-      <h1>
+    <header className={styles.header}>
+      <h1 className={styles.site_title}>
         <Link href="/">
           <a>Movie History</a>
         </Link>
       </h1>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/search">
-              <a>Search</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <button className={styles.nav_btn}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <Nav />
     </header>
   );
 };
